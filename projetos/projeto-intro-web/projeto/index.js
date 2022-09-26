a988e3bd203c1d5d296f83d0e02f21a2c5731e15
sampguilherme <guilherme.sampaio324@hotmail.com>
2022-09-26 13:55:18 -0300

    Atualização exercicios

--- projetos/projeto-intro-web/projeto/index.js
+++ projetos/projeto-intro-web/projeto/index.js
@@ -26,36 +26,18 @@ const jogos = [
 
 ]
 
-    mediaAnosEntreLancamento = (4 + 1 + 3) / 4
+let mediaAnosEntreLancamento = (4 + 1 + 3) / 4
 let jogosGratuitos = []
 let naoGratuitos = []
 for(let i in jogos){
     jogos[i].jogoGratuito ? jogosGratuitos.push(jogos[i]) : naoGratuitos.push(jogos[i])
 } 
 
-//console.log(`Media entre os anos de lançamentos de um jogo e outro: ${mediaAnosEntreLancamento} anos`)
-//console.log(`Todos são gratuitos para jogar? ${jogoGratuito && jogoGratuito2 && jogoGratuito3 && jogoGratuito4}`) // Verifica se todos jogos são gratuitos para jogar
+for(jogo of jogos){
+    for(propriedade in jogo){
+        console.log(`${propriedade}: ${jogo[propriedade]}`)
+    }
+console.log("=========================================")
+}
 
-console.log("Jogos gratuitos:", jogosGratuitos)
-
-console.log("===================================================")
-
-console.log("Jogos pagos:", naoGratuitos)
-
-/* console.log(`Jogo: ${jogo2.toUpperCase()}
-Ano de lançamento: ${anoLancamento2}
-Gratuito para jogar? ${jogoGratuito2}
-Plataformas disponiveis: ${plataformas2}
-`)
-
-console.log(`Jogo: ${jogo3.toUpperCase()}
-Ano de lançamento: ${anoLancamento3}
-Gratuito para jogar? ${jogoGratuito3}
-Plataformas disponiveis: ${plataformas3}
-`)
-
-console.log(`Jogo: ${jogo4.toUpperCase()}
-Ano de lançamento: ${anoLancamento4}
-Gratuito para jogar? ${jogoGratuito4}
-Plataformas disponiveis: ${plataformas4}
-`) */
+function funcJogo ()
